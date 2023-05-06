ab725cdc387bd48df4f24c4a438c7a04fc8666cd
Sean Kipinä <paintballsean@gmail.com>
2023-05-07 00:26:36 +0300

    Task 2 created car class

--- index.js
+++ index.js
@@ -81,7 +81,15 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
 }
 
 /*
