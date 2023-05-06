852749792139c6abf29f47cf5848194eab5f7a36
Sean Kipinä <paintballsean@gmail.com>
2023-05-07 00:28:59 +0300

    Task 2 added other elements to constructor.

--- index.js
+++ index.js
@@ -87,8 +87,21 @@ class Car {
     this.tank = 0;
     this.odometer = 0;
   }
+
   fill(gallons) {
     this.tank += gallons;
+  }
+
+  drive(distance) {
+    const maxDistance = this.tank * this.milesPerGallon;
+    if (distance <= maxDistance) {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    } else {
+      this.odometer += maxDistance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
   }
 }
 
