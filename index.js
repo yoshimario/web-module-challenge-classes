4e8811b456ea20d3058de1348b466b545d5696d6
Sean Kipinä <paintballsean@gmail.com>
2023-05-07 22:58:47 +0300

    Task 6 complete

--- index.js
+++ index.js
@@ -223,9 +223,22 @@ class Student extends Lambdasian {
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
+class ProjectManager extends Instructor {
+  constructor({ gradClassName, favInstructor, ...rest }) {
+    super(rest);
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+  }
+
+  standUp(slackChannel) {
+    return `${this.name} announces to ${slackChannel}, @channel standy times!`;
+  }
+
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
 }
+
 
 /*
   STRETCH PROBLEM (no tests!)
